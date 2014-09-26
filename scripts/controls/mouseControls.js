2e7ffa3ab4bd128f900c6aa317e4a83f7cafd954
bseay <bseay@bluvue.com>
2014-09-26 10:08:50 -0500

    MouseControls jsLint fixes
    fixed issues found by jslint

--- scripts/controls/mouseControls.js
+++ scripts/controls/mouseControls.js
@@ -1,93 +1,108 @@
-BluVueSheet.MouseControls = function (tileView) {
+BluVueSheet.MouseControls = function(tileView) {
     'use strict';
-	
-	var mouseStartX = 0;
-	var mouseStartY = 0;
-	var tileviewStartX = 0;
-	var tileviewStartY = 0;
-	var dragging = false;
-	function preventDefault(e) {
-	    e = e || window.event;
-	    if (e.preventDefault)
-	        e.preventDefault();
-	    e.returnValue = false;
-	}
 
-	this.onmousewheel = function(e){
-		var mouse = mouseLoc(e);
-		
-		var delta = wheelDistance(e);
-		var newScale = tileView.scale * (1.0 + (delta / 15));
-	    
-		if (newScale >= MIN_SCALE && newScale <= MAX_SCALE) {
-		    tileView.scale = newScale;
+    var mouseStartX = 0;
+    var mouseStartY = 0;
+    var tileviewStartX = 0;
+    var tileviewStartY = 0;
+    var dragging = false;
+
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault) {
+            e.preventDefault();
         }
 
-		var nx = e.clientX/tileView.scale-tileView.scrollX;
-		var ny = e.clientY/tileView.scale-tileView.scrollY;
-		//centers zoom around mouse
-		tileView.scrollX+=nx-mouse.x;
-		tileView.scrollY+=ny-mouse.y;
+        e.returnValue = false;
+    }
 
-		tileView.updateRes();
+    this.onmousewheel = function(e) {
+        var mouse = mouseLoc(e);
 
-	    preventDefault(e);
-	}
-	this.onmousedown = function(e){
-		tileView.colorMenu.hide();
-		var mouse = mouseLoc(e);
-		var windowX = e.clientX;
-		var windowY = e.clientY;
+        var delta = wheelDistance(e);
+        var newScale = tileView.scale * (1.0 + (delta / 15));
 
-		//Gesture things
-		mouseStartX=windowX;
-		mouseStartY=windowY;
-		tileviewStartX=tileView.scrollX;
-		tileviewStartY=tileView.scrollY;
+        if (newScale >= MIN_SCALE && newScale <= MAX_SCALE) {
+            tileView.scale = newScale;
+        }
 
-		//Event things
-		tileView.annotationManager.onmousedown(mouse.x,mouse.y);
+        var nx = e.clientX / tileView.scale - tileView.scrollX;
+        var ny = e.clientY / tileView.scale - tileView.scrollY;
+        //centers zoom around mouse
+        tileView.scrollX += nx - mouse.x;
+        tileView.scrollY += ny - mouse.y;
 
-		if(!tileView.annotationManager.captureMouse){
-			dragging=true;
-		}
+        tileView.updateRes();
 
-		preventDefault(e);
-	}
-	this.onmouseup = function(e){
-		var mouse = mouseLoc(e);
-		tileView.annotationManager.onmouseup(mouse.x,mouse.y);
-		dragging=false;
-	}
-	this.onmousemove = function(e){
-		var mouse = mouseLoc(e);
-		var window_x = e.clientX;
-		var window_y = e.clientY;
-		tileView.annotationManager.onmousemove(mouse.x,mouse.y);
-		if(dragging){
-			tileView.scrollX=tileviewStartX+(window_x-mouseStartX)/tileView.scale;
-			tileView.scrollY=tileviewStartY+(window_y-mouseStartY)/tileView.scale;
-		}
-	}
-	this.onclick = function(e){
-		var mouse = mouseLoc(e);
-		tileView.annotationManager.onclick(mouse.x,mouse.y);
-	}
-	this.ondblclick = function(e){
-		var mouse = mouseLoc(e);
-		tileView.annotationManager.ondblclick(mouse.x,mouse.y);
-	}
-	var wheelDistance = function(evt){
-		if (!evt) evt = event;
-		var w=evt.wheelDelta, d=evt.detail;
-		if (d){
-			if (w) return w/d/40*d>0?1:-1;
- 		else return -d/3;
-		} else return w/120;
-	}
-	function mouseLoc(e){
-		var x = e.clientX/tileView.scale-tileView.scrollX;
-		var y = e.clientY/tileView.scale-tileView.scrollY;
-		return new BluVueSheet.Point(x,y);
-	}
-}
+        preventDefault(e);
+    };
+
+    this.onmousedown = function(e) {
+        tileView.colorMenu.hide();
+        var mouse = mouseLoc(e);
+        var windowX = e.clientX;
+        var windowY = e.clientY;
+
+        //Gesture things
+        mouseStartX = windowX;
+        mouseStartY = windowY;
+        tileviewStartX = tileView.scrollX;
+        tileviewStartY = tileView.scrollY;
+
+        //Event things
+        tileView.annotationManager.onmousedown(mouse.x, mouse.y);
+
+        if (!tileView.annotationManager.captureMouse) {
+            dragging = true;
+        }
+
+        preventDefault(e);
+    };
+
+    this.onmouseup = function(e) {
+        var mouse = mouseLoc(e);
+        tileView.annotationManager.onmouseup(mouse.x, mouse.y);
+        dragging = false;
+    };
+
+    this.onmousemove = function(e) {
+        var mouse = mouseLoc(e);
+        var window_x = e.clientX;
+        var window_y = e.clientY;
+        tileView.annotationManager.onmousemove(mouse.x, mouse.y);
+        if (dragging) {
+            tileView.scrollX = tileviewStartX + (window_x - mouseStartX) / tileView.scale;
+            tileView.scrollY = tileviewStartY + (window_y - mouseStartY) / tileView.scale;
+        }
+    };
+
+    this.onclick = function(e) {
+        var mouse = mouseLoc(e);
+        tileView.annotationManager.onclick(mouse.x, mouse.y);
+    };
+
+    this.ondblclick = function(e) {
+        var mouse = mouseLoc(e);
+        tileView.annotationManager.ondblclick(mouse.x, mouse.y);
+    };
+
+    var wheelDistance = function(evt) {
+        if (!evt) { evt = event; }
+        var w = evt.wheelDelta, d = evt.detail;
+        if (d) {
+            if (w) {
+                return w / d / 40 * d > 0 ? 1 : -1;
+            } else {
+                return -d / 3;
+            }
+        } else {
+            return w / 120;
+        }
+    };
+
+    function mouseLoc(e) {
+        var x = e.clientX / tileView.scale - tileView.scrollX;
+        var y = e.clientY / tileView.scale - tileView.scrollY;
+        return new BluVueSheet.Point(x, y);
+    }
+};
