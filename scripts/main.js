a334e33faeb4d4cb1bc9e8af3ee2703d46f5bbc4
Alexey Beloglazov <beloglazov91@gmail.com>
2015-03-10 10:39:20 +0300

    BWA-1439 - Attachment Preview Window White Background

--- scripts/main.js
+++ scripts/main.js
@@ -765,6 +765,8 @@ angular.module("bluvueSheet").directive("bvSheet", ['$window', '$location', '$in
 			            filename: name
 		            };
 
+		            angular.element(document.querySelector('.bluvue-viewer-panel-holder')).addClass('bluvue-viewer-panel-holder-active');
+
 		            var panel_inline = document.getElementsByClassName('bluvue-viewer-panel-content-inline')[0];
 
 		            switch(type) {
@@ -830,6 +832,8 @@ angular.module("bluvueSheet").directive("bvSheet", ['$window', '$location', '$in
 		            scope.isShowViewerPlaceholder = false;
 
 		            angular.element(document.querySelector('.bluvue-viewer-panel-content')).css({height: 'auto', left: '-10000px'});
+
+		            angular.element(document.querySelector('.bluvue-viewer-panel-holder')).removeClass('bluvue-viewer-panel-holder-active');
 
 		            var image = document.getElementById('viewer-photo');
 		            image.style.display = 'none';
