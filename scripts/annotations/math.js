2422be340bd12e112075370cd493bd7e57468512
Tony Jacobs <tony.jacobs@gmail.com>
2015-03-29 23:36:32 -0400

    Lint cleanup in math.js.  Fixed logic issue when trying to use insets with poorly defined parameter sets (parameters that are null) - before, Rect.inset(0,1,1,1) would have been a NOOP (bug) instead of the rect inset adjustment.  This problem was not observed in the wild, so nothing obvious changed.

--- scripts/annotations/math.js
+++ scripts/annotations/math.js
@@ -1,59 +1,79 @@
 BluVueSheet.Point = function(x,y){
 	this.x=x;
 	this.y=y;
-}
+};
+
 BluVueSheet.Point.dist = function(p1, p2){
 	var dx = p1.x-p2.x;
 	var dy = p1.y-p2.y;
 	return Math.sqrt((dx*dx)+(dy*dy));
-}
+};
+
 BluVueSheet.ScaleMatrix = function(sx,sy,px,py){
 	this.applyTo = function(point){
 		var dx = point.x-px;
 		var dy = point.y-py;
 		point.x=px+(dx*sx);
 		point.y=py+(dy*sy);
-	}
-}
+	};
+};
+
 BluVueSheet.Rect = function(left,top,right,bottom){
 	this.left=left;
 	this.right=right;
 	this.top=top;
 	this.bottom=bottom;
+	
+	/**
+	 * Returns true if the given value is 
+	 **/
+	var isPoorlyDefined = function( val ) {
+	  return( val === null || val === undefined || isNaN(parseFloat(val)) && !isFinite(val) );
+	};
+	
 	this.width = function(){
 		return Math.abs(this.right-this.left);
-	}
+	};
+	
 	this.height = function(){
 		return Math.abs(this.bottom-this.top);
-	}
+	};
+	
 	this.centerX = function(){
 		return this.left+this.width()/2;
-	}
+	};
+	
 	this.centerY = function(){
 		return this.top+this.height()/2;
-	}
+	};
+	
 	this.inset = function(arg0,arg1,arg2,arg3){
-		if(arg0==null)return;
-		if(arg1==null){
+	  if( isPoorlyDefined( arg0 ) )
+		  return;
+		  
+		if( isPoorlyDefined( arg1 ) )
 			return this.inset(arg0,arg0,arg0,arg0);
-		}
-		if(arg2==null){
+
+		if( isPoorlyDefined( arg2 ) )
 			return this.inset(arg0,arg1,arg0,arg1);
-		}
+
 		var rect = this.clone();
 		rect.left+=arg0;
 		rect.top+=arg1;
 		rect.right-=arg2;
 		rect.bottom-=arg3;
 		return rect;
-	}
+	};
+	
 	this.clone = function(){
 	    return new BluVueSheet.Rect(this.left, this.top, this.right, this.bottom);
-	}
+	};
+	
 	this.containsPoint = function (point){
 		return contains(point.x,point.y);
-	}
+	};
+	
 	this.contains = function(x,y){
 		return Math.abs(x-this.centerX())<this.width()/2&&Math.abs(y-this.centerY())<this.height()/2;
-	}
-}
+	};
+};
