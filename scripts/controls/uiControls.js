b8eead45449c26e9d47e4171ba48283f0e74ee09
dima <dima.iluschenko@gmail.com>
2015-07-23 16:05:58 +0300

    BWA-2301 merge

--- scripts/controls/uiControls.js
+++ scripts/controls/uiControls.js
@@ -326,6 +326,16 @@ BluVueSheet.FloatingOptionsMenu = function (sheet, scope){
 			    addButton(BluVueSheet.Constants.OptionButtons.Delete);
 		    }
 
+      if (selectedAnnotations.length == 1 && ((selectedAnnotations[0].userId == null && scope.isAdmin) || (selectedAnnotations[0].userId != null && selectedAnnotations[0].userId == scope.userId))) {
+        addButton(BluVueSheet.Constants.OptionButtons.Link, function () {
+          if(selectedAnnotations[0].links.length) {
+            scope.showLinkPanel(false, true, null, null, true, true);
+          } else {
+            scope.showLinkPanel(true, true, null);
+          }
+        });
+      }
+
 	      if (selectedAnnotations.length == 1) {
             if(  (tileView.annotationManager.scaleAnnotation != null) )
             {
